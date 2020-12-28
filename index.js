beb542ea45e13b862a4d15a077c70a9fe09723d1
Mystery3051 <alexandr.job@yahoo.com>
2020-12-27 23:51:18 -0500

    Added image support to blogs.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const path = require("path")
 const express = require("express")
 const bodyParser = require("body-parser")
+const fileUpload = require("express-fileupload")
 const mongoose = require("mongoose")
 const ejs = require("ejs")
 const BlogPost = require("./models/BlogPost")
@@ -10,6 +12,7 @@ const app = new express()
 app.set("view engine", "ejs")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload())
 app.use(express.static("public"))
 
 app.listen(4000, ()=>{
@@ -38,7 +41,13 @@ app.get("/posts/new", (req, res) => {
     res.render("create")
 })
 
-app.post("/posts/store", async (req, res) => {
-    await BlogPost.create(req.body)
-    res.redirect("/")
+app.post("/posts/store", (req, res) => {
+    let image = req.files.image
+    image.mv(path.resolve(__dirname, "public/img", image.name), async error => {
+        await BlogPost.create({
+            ...req.body,
+            image: `/img/${image.name}`
+        })
+        res.redirect("/")
+    })
 })
