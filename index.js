ceeb65f451cbe42771bdfe6ceaa0632aa42f7b5c
Mystery3051 <alexandr.job@yahoo.com>
2020-12-27 23:38:31 -0500

    Updated blog posts schema.

--- index.js
+++ index.js
@@ -16,14 +16,20 @@ app.listen(4000, ()=>{
     console.log("App listening on port 4000")
 })
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    const blogposts = await BlogPost.find({})
+    res.render("index", {
+        blogposts
+    })
 })
 app.get("/about", (req, res) => {
     res.render("about")
 })
-app.get("/post", (req, res) => {
-    res.render("post")
+app.get("/post/:id", async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render("post", {
+        blogpost
+    })
 })
 app.get("/contact", (req, res) => {
     res.render("contact")
@@ -32,8 +38,7 @@ app.get("/posts/new", (req, res) => {
     res.render("create")
 })
 
-app.post("/posts/store", (req, res) => {
-    BlogPost.create(req.body, (error, blogpost) => {
-        res.redirect("/")
-    })
+app.post("/posts/store", async (req, res) => {
+    await BlogPost.create(req.body)
+    res.redirect("/")
 })
