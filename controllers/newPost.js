9ba543df2997ad5507e9d946381367b3dc8305b1
Mystery3051 <alexandr.job@yahoo.com>
2020-12-28 14:01:38 -0500

    Added config, deployed db.

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,6 +1,9 @@
 module.exports = (req, res) => {
     if (req.session.userId) {
-        return res.render("create")
+        return res.render("create", {
+            // Add necessary libraries to allow for post creation in WYSIWYG editor
+            createPost: true
+        })
     }
     
     res.redirect("auth/login")
