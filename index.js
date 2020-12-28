d56ff4ba330e45cf932cf65bb23f150bc1891f3c
Mystery3051 <alexandr.job@yahoo.com>
2020-12-27 19:05:12 -0500

    Added static files.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// const http = require("http")
-// const fs = require("fs")
 const express = require("express")
 const path = require("path")
 
 const app = express()
+app.use(express.static("public"))
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
